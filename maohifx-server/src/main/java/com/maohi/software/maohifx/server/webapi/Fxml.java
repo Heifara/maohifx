35b6d75f6643ed1d798a0555deaa5735a997976d
Heifara MATAPO <heifaramatapo@gmail.com>
2015-07-20 12:35:43 -1000

    Add Fxml class that dispatch url with fxml files Implement index.fxml and Controller.js

---
+++ maohifx-server/src/main/java/com/maohi/software/maohifx/server/webapi/Fxml.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.maohi.software.maohifx.server.webapi;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * @author heifara
+ *
+ */
+@Path("fxml")
+public class Fxml {
+	private static final HashMap<String, String> myMap;
+
+	static {
+		myMap = new HashMap<String, String>();
+		myMap.put("index", "index");
+	}
+
+	@Context
+	ServletContext context;
+
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response fromQueryParam(@QueryParam("id") String aId) {
+		return loadFxml(aId);
+	}
+
+	/**
+	 * @param aId
+	 * @return
+	 */
+	private Response loadFxml(String aId) {
+		try {
+			return Response.ok().entity(load(new URL("file:///" + context.getRealPath("/" + myMap.get(aId) + ".fxml").replace('\\', '/')))).build();
+		} catch (URISyntaxException | IOException aException) {
+			aException.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	private String load(URL aUrl) throws URISyntaxException, IOException {
+		StringBuilder iContent = new StringBuilder();
+
+		BufferedReader iBufferedReqder = new BufferedReader(new FileReader(new File(aUrl.toURI())));
+		String iLine;
+
+		while ((iLine = iBufferedReqder.readLine()) != null) {
+			iContent.append(iLine);
+		}
+
+		iBufferedReqder.close();
+		return iContent.toString();
+	}
+}
---
+++ maohifx-server/src/main/webapp/Controller.js
@@ -0,0 +1,96 @@
+load("fx:base.js");
+load("fx:controls.js");
+load("fx:graphics.js");
+
+var FXMLLoader = javafx.fxml.FXMLLoader;
+var URL = java.net.URL;
+
+/**
+ * Open a new Tab to home
+ * 
+ * @param aEvent
+ */
+function newTabEvent(aEvent) {
+	newTab("");
+}
+
+/**
+ * Open a new Tab
+ * 
+ * @param aEvent
+ *            the event
+ */
+function newTab(aUrl) {
+	var iNewTab = new Tab();
+	iNewTab.setText("Nouvelle Onglet");
+	//iNewTab.setContent(loadUrl("http://localhost:8080/webfx/webapi/fxml?id=tab", aUrl));
+
+	tabpane.getTabs().add(iNewTab);
+	tabpane.getSelectionModel().select(tabpane.getTabs().indexOf(iNewTab));
+}
+
+/**
+ * Using the FXMLLoader, load the url and put items
+ * 
+ * @param aUrl
+ *            the url
+ */
+function loadUrl(aUrl, aDefaultUrl) {
+	try {
+		iLoader = new FXMLLoader(new URL(aUrl));
+		iLoader.getNamespace().put("menuBar", menuBar);
+		iLoader.getNamespace().put("tabpane", tabpane);
+		iLoader.getNamespace().put("defaultUrl", aDefaultUrl);
+		return iLoader.load();
+	} catch (e) {
+		print(e.getMessage());
+	}
+}
+
+/**
+ * Close the current tab
+ */
+function closeCurrentTabEvent(aEvent) {
+	tabpane.getTabs().remove(tabpane.getTabs().indexOf(tabpane.getSelectionModel().getSelectedItem()))
+}
+
+function refreshCurrentTabEvent(event) {
+	iCurrentTab = tabpane.getSelectionModel().getSelectedItem();
+	url = iCurrentTab.getContent().getScene().lookup("#url");
+	content = iCurrentTab.getContent().getScene().lookup("#content");
+	if (!url.getText().isEmpty()) {
+		content.setCenter(loadUrl(url.getText(), ""));
+	}
+}
+
+/**
+ * @param aEvent
+ */
+function refreshEvent(aEvent) {
+	refresh();
+}
+
+function refresh() {
+	if (!url.getText().isEmpty()) {
+		content.setCenter(loadUrl(url.getText(), ""));
+	}
+}
+
+function homeEvent(aEvent) {
+	url.setText("http://localhost:8080/webfx/webapi/fxml?id=home");
+	refresh();
+}
+
+function aboutEvent(aEvent) {
+	newTab("http://localhost:8080/webfx/webapi/fxml?id=about");
+}
+
+function hideShowMenuBarEvent(aEvent) {
+	if (menuBar.isVisible()) {
+		menuBar.setVisible(false);
+		hideShowMenuBarMenuItem.setText("Afficher la barre de menu");
+	} else {
+		menuBar.setVisible(true);
+		hideShowMenuBarMenuItem.setText("Masquer la barre de menu");
+	}
+}
