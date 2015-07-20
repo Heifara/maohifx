6275695f7b5ca6f4b1ce5e95832ba35e6f9a2b4f
Heifara MATAPO <heifaramatapo@gmail.com>
2015-07-20 12:24:47 -1000

    create basic packages create basic Post, Get and MyApplication classes create basic web.xml

---
+++ maohifx-server/src/main/java/com/maohi/software/maohifx/server/webapi/MyApplication.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.maohi.software.maohifx.server.webapi;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * @author heifara
+ *
+ */
+public class MyApplication extends ResourceConfig {
+
+	public MyApplication() {
+	}
+
+}
