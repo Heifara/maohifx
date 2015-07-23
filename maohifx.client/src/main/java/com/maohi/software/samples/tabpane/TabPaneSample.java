924964685b58294edbe0320491c2877e95ea19b7
Heifara MATAPO <heifaramatapo@gmail.com>
2015-07-23 07:12:32 -1000

    create TabPaneSample

---
+++ maohifx.client/src/main/java/com/maohi/software/samples/tabpane/TabPaneSample.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.maohi.software.samples.tabpane;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * @author heifara
+ *
+ */
+public class TabPaneSample extends Application {
+
+	@Override
+	public void start(Stage aStage) throws Exception {
+		TabPane iTabPane = new TabPane();
+		iTabPane.getTabs().add(new Tab("Nouvelle Onglet"));
+
+		BorderPane iRoot = new BorderPane(iTabPane);
+		aStage.setScene(new Scene(iRoot, 600, 400));
+		aStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
