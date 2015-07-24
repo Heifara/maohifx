37ce55e2a48408ee2c5b3e8dbff89d5892795a8a
Heifara MATAPO <heifaramatapo@gmail.com>
2015-07-23 15:15:33 -1000

    update home/ to test $http

--- maohifx.server/src/main/webapp/home/Controller.js
+++ maohifx.server/src/main/webapp/home/Controller.js
@@ -2,6 +2,43 @@ load("http://localhost:8080/maohifx.server/common.js");
 
 function HomeController() {
 	$tab.setText("Home");
+
+	iButton = new Button("Get");
+	iButton.onAction = function() {
+		iResponse = $http.get("http://localhost:8080/maohifx.server/webapi/get");
+		print(iResponse);
+	}
+	$toolbar.getItems().add(iButton);
+
+	iButton = new Button("Get JSON");
+	iButton.onAction = function() {
+		iResponse = $http.get({
+			url : "http://localhost:8080/maohifx.server/webapi/get",
+			data : {
+				value1 : "test",
+				value2 : 2.2
+			}
+		});
+		print(iResponse);
+	}
+	$toolbar.getItems().add(iButton);
+
+	iButton = new Button("Post");
+	iButton.onAction = function() {
+		iResponse = $http.post("http://localhost:8080/maohifx.server/webapi/post");
+		print(iResponse);
+	}
+	$toolbar.getItems().add(iButton);
+
+	iButton = new Button("Post JSON");
+	iButton.onAction = function() {
+		iResponse = $http.get({
+			url : "http://localhost:8080/maohifx.server/webapi/post",
+			data : ""
+		});
+		print(iResponse);
+	}
+	$toolbar.getItems().add(iButton);
 }
 
 HomeController.prototype.openEvent = function(aEvent) {
