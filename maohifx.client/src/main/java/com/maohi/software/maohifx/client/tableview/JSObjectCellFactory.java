bf09b0665659c71229d5ea69e3f44194b07a0a97
Heifara MATAPO <heifaramatapo@gmail.com>
2015-07-30 21:24:54 -1000

    Fix bug in JSObjectCellFactory where some type were not handle. Also create inner class JSObjectStringConverter

--- maohifx.client/src/main/java/com/maohi/software/maohifx/client/tableview/JSObjectCellFactory.java
+++ maohifx.client/src/main/java/com/maohi/software/maohifx/client/tableview/JSObjectCellFactory.java
@@ -15,29 +15,51 @@ import javafx.util.StringConverter;
  */
 public class JSObjectCellFactory<S, T> implements Callback<TableColumn<S, T>, TableCell<S, T>> {
 
+	/**
+	 * @author heifara
+	 *
+	 */
+	public class JSObjectStringConverter extends StringConverter<T> {
+
+		private Class<?> type;
+
+		@Override
+		public String toString(T aObject) {
+			if (aObject != null) {
+				if (aObject instanceof String) {
+					type = String.class;
+					return (String) aObject;
+				} else if (aObject instanceof Double) {
+					type = Double.class;
+					return aObject.toString();
+				} else if (aObject instanceof Float) {
+					type = Float.class;
+					return aObject.toString();
+				} else {
+					return aObject.toString();
+				}
+			}
+			return null;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public T fromString(String aString) {
+			if (type == String.class) {
+				return (T) aString;
+			} else if (type == Double.class) {
+				return (T) Double.valueOf(aString);
+			} else if (type == Float.class) {
+				return (T) Float.valueOf(aString);
+			} else {
+				throw new IllegalArgumentException(String.format("The type of %s is illegal", aString));
+			}
+		}
+	}
+
 	@Override
 	public TableCell<S, T> call(final TableColumn<S, T> param) {
-		return new TextFieldTableCell<S, T>(this.newStringConverter());
+		return new TextFieldTableCell<S, T>(new JSObjectStringConverter());
 	}
 
-	private StringConverter<T> newStringConverter() {
-		return new StringConverter<T>() {
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public T fromString(final String aString) {
-				return (T) aString;
-			}
-
-			@Override
-			public String toString(final T aObject) {
-				if (aObject != null) {
-					if (aObject instanceof String) {
-						return (String) aObject;
-					}
-				}
-				return null;
-			}
-		};
-	}
 }
