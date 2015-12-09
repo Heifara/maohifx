95e793614f7a71c31d93d2ab3279f99dfa6f6516
Heifara MATAPO <heifaramatapo@gmail.com>
2015-12-09 09:48:30 -1000

    Move fxml protocol handler from maohifx.client to maohifx.common

--- maohifx.control/src/test/java/com/maohi/software/maohifx/beans/Person.js
+++ maohifx.control/src/test/java/com/maohi/software/maohifx/beans/Person.js
@@ -2,4 +2,5 @@ function Person() {
 	this.firstName = new SimpleStringProperty();
 	this.lastName = new SimpleStringProperty();
 	this.href = new SimpleStringProperty();
+	this.age = new SimpleDoubleProperty();
 }
--- maohifx.control/src/test/java/com/maohi/software/maohifx/control/FXMLTableViewTest.js
+++ maohifx.control/src/test/java/com/maohi/software/maohifx/control/FXMLTableViewTest.js
@@ -8,12 +8,14 @@ function FXMLTableViewTest() {
 	iPerson = new Person();
 	iPerson.firstName.set("Jacob");
 	iPerson.lastName.set("Smith")
+	iPerson.age.set(16.0);
 	iPerson.href.set("http://localhost:8080/maohifx.server/webapi/fxml?id=index");
 	this.data.add(iPerson);
 
 	iPerson = new Person();
 	iPerson.firstName.set("Isabella");
 	iPerson.lastName.set("Johnson")
+	iPerson.age.set(15.0);
 	iPerson.href.set("http://localhost:8080/maohifx.server/webapi/fxml?id=index");
 	this.data.add(iPerson);
 
