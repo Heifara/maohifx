c78c07a413443bddae4d72b0b9270f97174c566f
Heifara MATAPO <heifaramatapo@gmail.com>
2015-12-09 09:47:20 -1000

    Implement BindingSample

---
+++ maohifx.samples/src/main/java/com/maohi/software/maohifx/samples/BindingSample.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package com.maohi.software.maohifx.samples;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+/**
+ * @author heifara
+ *
+ */
+public class BindingSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final SimpleDoubleProperty num1 = new SimpleDoubleProperty(1.0);
+		final IntegerProperty num2 = new SimpleIntegerProperty(2);
+		final IntegerProperty num3 = new SimpleIntegerProperty(3);
+		final IntegerProperty num4 = new SimpleIntegerProperty(4);
+		final NumberBinding total = Bindings.add(num1.multiply(num2), num3.multiply(num4));
+
+		System.out.println(total.getValue());
+
+		num1.set(2);
+		System.out.println(total.getValue());
+	}
+
+}
