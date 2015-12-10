679a032241c0d4b8842a7f537331e21e180f0a67
Heifara MATAPO <heifaramatapo@gmail.com>
2015-12-10 11:17:59 -1000

    fix start.bat and recompile start.exe

--- package/start.bat
+++ package/start.bat
@@ -4,4 +4,4 @@ for /f "tokens=1,2 delims==" %%a in (config.ini) do (
 	if %%a==jrepath set jrepath=%%b
 )
 
-start %jrepath%\bin\java -classpath "lib/*" com.maohi.software.maohifx.client.Main
+start %jrepath%\bin\javaw -classpath "lib/*" com.maohi.software.maohifx.client.Main
