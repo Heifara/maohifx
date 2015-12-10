6ef2c2a97a6dbd2d3ae583e61953778949a73bd8
Heifara MATAPO <heifaramatapo@gmail.com>
2015-12-10 11:54:30 -1000

    Now, can configure debug mode in config.ini

--- package/start.bat
+++ package/start.bat
@@ -2,6 +2,9 @@
 
 for /f "tokens=1,2 delims==" %%a in (config.ini) do (
 	if %%a==jrepath set jrepath=%%b
+	if %%a==debug set debug=%%b
+	if %%a==port set port=%%b
 )
 
-start %jrepath%\bin\javaw -classpath "lib/*" com.maohi.software.maohifx.client.Main
+if %debug% == 1 %jrepath%\bin\java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=%port% -classpath "lib/*" com.maohi.software.maohifx.client.Main
+if %debug% == 0	start %jrepath%\bin\javaw -classpath "lib/*" com.maohi.software.maohifx.client.Main
