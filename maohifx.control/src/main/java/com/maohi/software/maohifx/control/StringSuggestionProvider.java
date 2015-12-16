b57375e63777813be63599a8754f0a3cbc8258b7
Heifara MATAPO <heifaramatapo@gmail.com>
2015-12-16 11:46:41 -1000

    Fix issue where suggestion was not changed when autocompletion list item changed

--- maohifx.control/src/main/java/com/maohi/software/maohifx/control/StringSuggestionProvider.java
+++ maohifx.control/src/main/java/com/maohi/software/maohifx/control/StringSuggestionProvider.java
@@ -3,24 +3,28 @@
  */
 package com.maohi.software.maohifx.control;
 
-import java.util.Collection;
 import java.util.Comparator;
 
 import org.controlsfx.control.textfield.AutoCompletionBinding.ISuggestionRequest;
 
 import impl.org.controlsfx.autocompletion.SuggestionProvider;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.util.StringConverter;
 
 /**
  * @author heifara
  *
  */
-public class StringSuggestionProvider<T> extends SuggestionProvider<T>implements Comparator<T> {
+public class StringSuggestionProvider<T> extends SuggestionProvider<T>implements Comparator<T>, ListChangeListener<T> {
 
 	private final StringConverter<T> stringConverter;
+	private final ObservableList<T> suggestion;
 
-	public StringSuggestionProvider(final Collection<T> aSuggestions, final StringConverter<T> aStringConverter) {
+	public StringSuggestionProvider(final ObservableList<T> aSuggestions, final StringConverter<T> aStringConverter) {
 		this.stringConverter = aStringConverter;
+		this.suggestion = aSuggestions;
+		this.suggestion.addListener(this);
 
 		this.addPossibleSuggestions(aSuggestions);
 	}
@@ -42,5 +46,14 @@ public class StringSuggestionProvider<T> extends SuggestionProvider<T>implements
 		final String iUserTextLower = aRequest.getUserText().toLowerCase();
 		final String iSuggestionStr = this.stringConverter.toString(aSuggestion).toLowerCase();
 		return iSuggestionStr.contains(iUserTextLower) && !iSuggestionStr.equals(iUserTextLower);
+	}
+
+	@Override
+	public void onChanged(final javafx.collections.ListChangeListener.Change<? extends T> aChange) {
+		if (aChange.next()) {
+			this.clearSuggestions();
+
+			this.addPossibleSuggestions(this.suggestion);
+		}
 	}
 }
